de722671ca38bfa18f58fd94cfcae529c39ff21f
UrsusFeline <ursusfeline07@gmail.com>
2023-07-24 15:57:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/env bash
-
+source config/config.sh
 ## 雨云mcsm面板服务器请删除下一行注释
 #chown -R root:root /home/container
 
 ./git.sh start
 cd work_dir
-LD_LIBRARY_PATH=. ./bedrock_server
+exec_server
 cd ..
 ./git.sh stop
